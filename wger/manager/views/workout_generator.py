5e013da552267bb473f097e521ad2e6da26bd5fd
vp <vpatel@localhost.localdomain>
2021-03-05 00:15:19 -0800

    - implemented logic for getting desired workout types - implemented logic for outputting sample workouts - still need a few fixes

--- wger/manager/views/workout_generator.py
+++ wger/manager/views/workout_generator.py
@@ -7,14 +7,51 @@ from django.shortcuts import (
 )
 from wger.manager.forms import (
     WorkoutGeneratorForm,
-    WorkoutScheduleDownloadForm
+    WorkoutGeneratorResults
 )
 
-from wger.exercises.models import (
-    ExerciseCategory,
+from wger.exercises.models import Exercise
 
-    Exercise
-)
+from random import shuffle
+
+
+def getGeneratorParams(postData):
+    return postData['categories'], postData['levels']
+
+
+def generate_plan(exercise_list, low_range, high_range):
+    pairs = []
+    exercise_number = 1
+    for exercise in exercise_list:
+        new_tuple = (exercise_number, exercise.name, str(low_range), str(high_range))
+        pairs.append(new_tuple)
+        exercise_number += 1
+    return pairs
+
+
+def generate(cat, lev):
+    levelVal = 1 # beginner
+    if (lev == "Intermediate"):
+        levelVal = 2
+    else:
+        levelVal = 3
+
+
+    # first get all workouts of the appropriate category
+    exercises = list(Exercise.objects.filter(category__name=cat, language__short_name="en"))
+    # debug
+    # for exercise in exercises:
+    #     if exercise.language.short_name == "en":
+    #         print(exercise.name)
+
+    # set the constant values
+    num_exercises = levelVal * 3
+    rep_range_low = levelVal * 5
+    rep_range_high = levelVal * 10
+
+    exercise_set = exercises[:num_exercises]
+    shuffle(exercise_set)
+    return generate_plan(exercise_set, rep_range_low, rep_range_high)
 
 @login_required
 def overview(request):
@@ -22,15 +59,26 @@ def overview(request):
     An overview of the workout-generator page
     """
     template_data = {}
-    data = {}
+    # check if POST or GET
     if request.method == 'POST':
-        data['exercises']= list(Exercise.objects.filter(category__name=request.POST['categories']))
-        form = WorkoutGeneratorForm(request.POST, initial=data)
+        # get the original form with users selections
+        form = WorkoutGeneratorForm(request.POST)
+
+        # begin generating the results
+        result_data = {}
+        result_data['category'], result_data['level'] = getGeneratorParams(request.POST)
         template_data['form'] = form
-        template_data['popup'] = WorkoutScheduleDownloadForm()
+
+        # generate workouts
+        result_data['workout_plan'] = generate(result_data['category'], result_data['level'])
+
+        for elem in result_data['workout_plan']:
+            print(elem)
+
+        template_data['results'] = result_data
     else:
         # method == 'GET'
-        form = WorkoutGeneratorForm(initial=data)
+        form = WorkoutGeneratorForm()
         template_data['form'] = form
 
     return render(request, 'generator/generator.html', template_data)
